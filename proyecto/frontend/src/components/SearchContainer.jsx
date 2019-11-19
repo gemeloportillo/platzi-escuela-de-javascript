12bc0c73ce2f9a58bdc36fe373627fa9eb41ed4d
Miguel Angel Portillo Bobadilla <gemeloportillo@yahoo.com>
2019-11-19 01:25:29 -0600

    Add SearchContainer.jsx component

--- proyecto/frontend/src/components/SearchContainer.jsx
+++ proyecto/frontend/src/components/SearchContainer.jsx
@@ -5,7 +5,7 @@ import '../assets/styles/iconos.scss';
 
 const SearchContainer = () => (
   <div id='searchContainer' className='search-container'>
-    <form action=''>
+    <form>
       <div className='icono'><div className='icon icon-magnifier-tool' /></div>
       <input type='text' placeholder='Busca tu mueble...' name='search' />
     </form>
